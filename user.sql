2bdd1cba118b0b754cca6e4c03ff7beb125fc048
CG166 <89878026+CG166@users.noreply.github.com>
2024-12-04 12:29:36 -0800

    Add files via upload

---
+++ user.sql
@@ -0,0 +1,26 @@
+USE poemsy;
+
+CREATE TABLE Users (
+first_name varchar(255) NOT NULL,
+last_name varchar(255) NOT NULL,
+user_id INT PRIMARY KEY auto_increment,
+username varchar(255) NOT NULL,
+email varchar(255) unique NOT NULL,
+password varchar(255) NOT NULL
+);
+
+CREATE TABLE Posts (
+poem_id INT PRIMARY KEY auto_increment,
+title varchar(255) NOT NULL,
+category ENUM('Romance', 'War', 'Fame', 'Parenthood', 'Loss') NOT NULL,
+content TEXT NOT NULL,
+user_id INT NOT NULL,
+FOREIGN KEY (user_id) REFERENCES Users(user_id)
+)ENGINE=InnoDB;
+
+CREATE TABLE Likes (
+user_id INT NOT NULL,
+poem_id INT NOT NULL,
+FOREIGN KEY (user_id) REFERENCES Users(user_id),
+FOREIGN KEY (poem_id) REFERENCES Posts(poem_id)
+)ENGINE=InnoDB;
